724e9aef68701c71ccec5604ab8dcad224ab1d83
Binn <126502493+Binn1908@users.noreply.github.com>
2023-07-18 09:40:06 +0200

    Update tab_analyse.py

--- tab_analyse.py
+++ tab_analyse.py
@@ -1,8 +1,7 @@
 import streamlit as sl
 import pandas as pd
-#import pickle
 import plotly.express as px
-#import WordCloud
+from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
 @sl.cache_data
@@ -17,18 +16,17 @@ def load_df():
 df = load_df()
 
 def tab_analyse():
-	sl.title("page analyse")
-
-	#sl.dataframe(df)
+	sl.title("Analyse")
 
 	df_country = df.groupby(by = ['country', 'code'], as_index = False)['points'].mean()
-	fig = px.choropleth(df_country, locations='code', color='points', hover_name='country', title='test')
+	fig = px.choropleth(df_country, locations = 'code', color = 'points', hover_name = 'country', title = 'test')
 	#projection='natural earth'
 	sl.plotly_chart(fig)
 
-	#wordcloud = WordCloud(width = 480, height = 480, max_font_size = 200, min_font_size = 10)
-	#wordcloud.generate_from_text(" ".join(df['description_clean']))
-	#fig, ax = plt.subplots()
-	#ax.imshow(wordcloud, interpolation = "bilinear")
-	#plt.axis("off")
-	#sl.pyplot(fig)
+	wordcloud = WordCloud(width = 480, height = 480, max_font_size = 200, min_font_size = 10)
+	wordcloud.generate_from_text(" ".join(df['description_clean']))
+	fig, ax = plt.subplots()
+	ax.imshow(wordcloud, interpolation = "bilinear")
+	plt.axis("off")
+	#plt.margins(x = 0, y = 0)
+	sl.pyplot(fig)
