eadbff52efae7370e1903ed21ba7d0f27e45af8b
Binn <126502493+Binn1908@users.noreply.github.com>
2023-07-18 09:37:00 +0200

    Update tab_home.py

--- tab_home.py
+++ tab_home.py
@@ -1,8 +1,10 @@
 import streamlit as sl
+from PIL import Image
 
 sl.set_page_config(layout = 'wide')
 
 def tab_home():
 	sl.title("Bienvenue")
 
-	#afficher photo vignes
+	affiche = Image.open('vigne.jpeg')
+	sl.image(affiche)
