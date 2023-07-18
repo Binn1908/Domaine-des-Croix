2dfd78030aa79235f90965ffd2adc89166e01e98
Binn <126502493+Binn1908@users.noreply.github.com>
2023-07-18 19:11:55 +0200

    Update tab_home.py

--- tab_home.py
+++ tab_home.py
@@ -4,7 +4,7 @@ from PIL import Image
 sl.set_page_config(layout = 'wide')
 
 def tab_home():
-	sl.title("Bienvenue")
+	sl.title('Bienvenue')
 
-	affiche = Image.open('vigne.jpeg')
-	sl.image(affiche)
+	img = Image.open('vigne.png')
+	sl.image(img)
